a122c85c044a45bb24c21fe26bf9cffa820fd1e5
BingqiChen <bingqichen@live.cn>
2016-06-21 09:55:38 +0800

    pop

--- src/js/popover.js
+++ src/js/popover.js
@@ -1,6 +1,6 @@
 ;(function (Vue, window) {
   var template = `
-  <div class="popover-wrap" v-show="show" transition="popover">
+  <div class="popover-wrap" v-show="show" transition="popover" :style="{left: position.x + 'px', top: position.y + 'px'}">
     <div class="popover-box">
       <div class="popover-title">{{title}}</div>
       <div class="popover-content">{{content}}</div>
@@ -21,9 +21,22 @@
       content: {
         type: String,
         default: '内容'
+      },
+      position: {
+        type: Object,
+        default: {}
       }
     }
   })
 
+  function calcPosition(e) {
+    var target = e.target
+    return {
+      x: target.offsetLeft,
+      y: target.offsetTop + target.scrollHeight + 5
+    }
+  }
+
   window.popover = popover
+  window.calc = calcPosition
 })(Vue, window)

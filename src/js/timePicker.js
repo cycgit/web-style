e76e2e5f256d0a0c14f7e1d788db3bed6b5f3ef4
cyc <342930982@qq.com>
2016-06-21 18:09:27 +0800

    时间组件

--- src/js/timePicker.js
+++ src/js/timePicker.js
@@ -1,4 +1,15 @@
 (function (Vue, components) {
+
+  /**
+   *
+   * @params val (08:20:30)
+   *
+   * @events time-change(val)
+   */
+
+
+
+
   let template = ` <div class="tp">
                     <div class="tp-header">
                         <input placeholder="请选择时间" :value="out">
@@ -29,23 +40,31 @@
     props: {
       val: {
         coerce: function (val) {
-          // console.log(val)
+
+          if (val.indexOf(':') != -1) {
+
+            // val = ('0' + n.getHours()).slice(-2) + ':' + ('0' + n.getMinutes()).slice(-2) + ':' + ('0' + n.getSeconds()).slice(-2)
+            let sp = val.split(':')
+            val = parseInt(sp[0]) + ':' + parseInt(sp[1]) + ':' + parseInt(sp[2])
+          }
+
+
+
           return val
         }
       }
     },
     data(){
 
-      console.log(this.val)
-
-
       var sp = this.val.split(':')
 
+      if (!this.val) sp = [-1, -1, -1]
+
       return {
-        val: this.val,
         sh: sp[0],
         sm: sp[1],
-        ss: sp[2]
+        ss: sp[2],
+        op: false
       }
     },
     ready(){
@@ -64,23 +83,45 @@
         return 60
       },
       out(){
-        return `${this.sh}:${this.sm}:${this.ss}`
+        if(!this.val) return ''
+        let sp = this.val.split(':')
+
+        return ('0' + sp[0]).slice(-2) + ':' + ('0' + sp[1]).slice(-2) + ':' + ('0' + sp[2]).slice(-2)
       }
     },
     watch: {
-      sh(n){
-        this.scroll(n, 'h')
-      },
-      sm(n){
-        this.scroll(n, 'm')
-      },
-      ss(n){
-        this.scroll(n, 's')
+      val(val){
+        if(this.op){
+         
+          this.$dispatch('time-change', this.val)
+
+        }else{
+          //外部赋值
+            if(val){
+              let sp = val.split(':')
+              this.sh = parseInt(sp[0])
+              this.sm = parseInt(sp[1])
+              this.ss = parseInt(sp[2])
+              this.$els.h.scrollTop = this.sh * 24
+              this.$els.m.scrollTop = this.sm * 24
+              this.$els.s.scrollTop = this.ss * 24
+            }else{
+                this.clear()
+            }
+
+        }
+
+        this.op = false
+
       }
     },
     methods: {
       click(num, type){
+        this.op = true
         this['s' + type] = num
+        this.scroll(num, type)
+        this.change()
+
       },
       scroll(num, type){
 
@@ -101,7 +142,30 @@
         }
       },
       change(){
+        if (this.sh == -1) {
+          this.sh = 0
+        }
+        if (this.sm == -1) {
+          this.sm = 0
+        }
+        if (this.ss == -1) {
+          this.ss = 0
+        }
 
+        let val = ('0' + this.sh).slice(-2) + ':' + ('0' + this.sm).slice(-2) + ':' + ('0' + this.ss).slice(-2)
+
+        this.val = val
+        
+      },
+      clear(){
+        this.sh = -1
+        this.sm = -1
+        this.ss = -1
+        this.op = false
+        this.$els.h.scrollTop = 0
+        this.$els.m.scrollTop = 0
+        this.$els.s.scrollTop = 0
+        this.val = ''
       }
     }
   })

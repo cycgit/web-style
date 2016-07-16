f53a28ed5237bce01deddacc1fdd45925829b040
cyc <342930982@qq.com>
2016-07-16 17:26:14 +0800

    引入vue-cli配置

---
+++ build/com.js
@@ -0,0 +1,40 @@
+// https://github.com/shelljs/shelljs
+require('shelljs/global')
+env.NODE_ENV = 'production'
+
+var path = require('path')
+var config = require('../config')
+var ora = require('ora')
+var webpack = require('webpack')
+var webpackConfig = require('./webpack.com.conf')
+
+var assetsPath = path.join(config.com.assetsRoot, config.com.assetsSubDirectory)
+console.log(assetsPath)
+
+
+
+console.log(
+  '  Tip:\n' +
+  '  Built files are meant to be served over an HTTP server.\n' +
+  '  Opening index.html over file:// won\'t work.\n'
+)
+
+var spinner = ora('building for production...')
+spinner.start()
+
+
+rm('-rf', assetsPath)
+mkdir('-p', assetsPath)
+cp('-R', assetsPath)
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})

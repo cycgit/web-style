3fbab10a71421d7bb624451f0564f2f88c794b50
BingqiChen <bingqichen@live.cn>
2016-07-23 11:02:41 +0800

    ok

---
+++ dist/js/static.js
@@ -0,0 +1,1 @@
+!function(r){function t(n){if(e[n])return e[n].exports;var o=e[n]={exports:{},id:n,loaded:!1};return r[n].call(o.exports,o,o.exports,t),o.loaded=!0,o.exports}var e={};return t.m=r,t.c=e,t.p="../",t(0)}([function(r,t,e){"use strict";e(1)},function(r,t){}]);
